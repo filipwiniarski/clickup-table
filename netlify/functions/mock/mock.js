4b5e537c97f3d4d45b1834603a01fba5dc44d599
Filip Winiarski <f.winiarski@tisagroup.ch>
2021-06-14 10:25:14 +0200

    fix: fix 502 server response

--- netlify/functions/mock/mock.js
+++ netlify/functions/mock/mock.js
@@ -1,8 +1,11 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const fetch = require("node-fetch");
+
+let url =
+  "http://clickup-miniproject-filipwiniarski.netlify.app/assets/data.json";
+
+let settings = { method: "Get" };
 
 const total = 80;
-
-const data = require("../../data");
 
 const headers = {
   "Access-Control-Allow-Origin": "*",
@@ -10,57 +13,44 @@ const headers = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
 };
 
-const handler = async (event) => {
-  console.log("cat");
-  const { size, page, query } = event.queryStringParameters;
+const handler = (event, context, callback) => {
+  fetch(url, settings)
+    .then((res) => res.json())
+    .then((json) => {
+      const { size, page, query } = event.queryStringParameters;
+      const _size = Number(size);
+      const _page = Number(page);
+      const start = _page * _size;
+      const take = start + _size >= total ? total : start + _size;
 
-  console.log(size, page, query);
-  const _size = Number(size);
-  const _page = Number(page);
-  const start = _page * _size;
-  const take = start + _size >= total ? total : start + _size;
+      let results;
 
-  console.log(size, page, query);
-
-  let results;
-
-  console.log(size, page, query, results);
-
-  try {
-    if (query?.length > 0 && query !== "null") {
-      console.log("xd");
-      results = data.data
-        .filter((item) => item.name.toLowerCase().includes(query.toLowerCase()))
-        .slice(start, take);
-      console.log(size, page, query, results);
-      return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({
-          data: results,
-          total: results.length,
-        }),
-      };
-    } else {
-      console.log("dog");
-      results = data.data.slice(start, take);
-      console.log(size, page, query, results);
-      return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({
-          data: results,
-          total: total,
-        }),
-      };
-    }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      headers,
-      body: error.toString(),
-    };
-  }
+      if (query?.length > 0 && query !== "null") {
+        results = json.data
+          .filter((item) =>
+            item.name.toLowerCase().includes(query.toLowerCase())
+          )
+          .slice(start, take);
+        callback(null, {
+          statusCode: 200,
+          headers,
+          body: JSON.stringify({
+            data: results,
+            total: results.length,
+          }),
+        });
+      } else {
+        results = json.data.slice(start, take);
+        callback(null, {
+          statusCode: 200,
+          headers,
+          body: JSON.stringify({
+            data: results,
+            total: total,
+          }),
+        });
+      }
+    });
 };
 
 module.exports = { handler };
